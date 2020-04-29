d0c6fa57f3f96b52c21002e18451617f159f7824
madhank93 <maddymadhan946@gmail.com>
2020-04-29 22:04:32 +0530

    no message

---
+++ android/app/src/main/java/io/madhank93/fluttermvpillustrativeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package io.madhank93.fluttermvpillustrativeapp;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
---
+++ lib/data/xkcd_data.dart
@@ -0,0 +1,5 @@
+class XKCD {
+  String id;
+  String imageURL;
+
+}
