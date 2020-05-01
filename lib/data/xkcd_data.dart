2f655033ee2fe35255fce5c00acc4837f625cd2b
madhank93 <maddymadhan946@gmail.com>
2020-05-01 21:24:47 +0530

    adding class to fetch network details

--- lib/data/xkcd_data.dart
+++ lib/data/xkcd_data.dart
@@ -2,4 +2,20 @@ class XKCD {
   String id;
   String imageURL;
 
+  XKCD(this.id, this.imageURL);
+}
+
+abstract class XKCDRepo {
+  Future<List<XKCD>> fetchComicPost();
+}
+
+class FetchDataException implements Exception {
+  final _message;
+
+  FetchDataException([this._message]);
+
+  String toString() {
+    if (_message == null) return "Exception";
+    return "Exception: $_message";
+  }
 }
---
+++ lib/data/xkcd_data_prod.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'package:fluttermvpillustrativeapp/data/xkcd_data.dart';
+
+class ProdXKCDRepo implements XKCDRepo {
+  String baseURL = "http://xkcd.com/";
+
+  @override
+  Future<List<XKCD>> fetchComicPost() async {
+    http.Response response = await http.get(baseURL + "info.0.json");
+    final List responseBody = jsonDecode(response.body);
+    final statusCode = response.statusCode;
+
+    if (statusCode != 200 || response.body == null) {
+      throw new FetchDataException("An error occured while fetching data: [Status Code: $statusCode]");
+    }
+    return responseBody.map((c) =>new XKCD.fromMap(c))
+  }
+}
